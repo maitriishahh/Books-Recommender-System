836ad3cfc8c0f3d0976524255e686a98f75bb3ec
maitriishahh <maitrishah142004@gmail.com>
2026-04-05 15:35:49 +0530

    tested data transformation module

--- books_recommender/pipeline/training_piepline.py
+++ books_recommender/pipeline/training_piepline.py
@@ -1,14 +1,16 @@
 from books_recommender.components.s1_data_ingestion import DataIngestion
 from books_recommender.components.s2_data_validation import DataValidation
+from books_recommender.components.s3_data_transformation import DataTransformation
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
+        self.data_transformation = DataTransformation()
 
     def start_training_pipeline(self):
         """starts the training pipeline 
         :return: none"""
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
-    
+        self.data_transformation.initiate_data_transformation()
